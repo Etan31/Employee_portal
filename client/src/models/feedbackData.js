b7c293ecb0ab7fd866366243d3b59d7e80c29fff
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2026-05-05 14:09:19 +0800

    Feat: added feedback panel

---
+++ client/src/models/feedbackData.js
@@ -0,0 +1,11 @@
+export const FEEDBACK_DATA = [
+  /* 
+  { 
+    id: '1', 
+    from: 'Marcus Chen', 
+    content: 'Great job on the latest dashboard update! The UI is much cleaner.', 
+    date: '2026-05-01',
+    type: 'Positive'
+  }
+  */
+];
